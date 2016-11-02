46b92b3524350e2bd02bbba3bcda00d35a91bc69
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2016-11-02 21:47:17 +0100

    edit: site's architecture

--- index.php
+++ index.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-      <meta charset="utf-8" />
-      <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-      <!-- Optional theme -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-      <!-- Latest compiled and minified JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-      <script src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous">
-        </script>
-      <title>Accueil</title>
-    </head>
+<?php
+/**
+* Ajout des variables globales pour les emplacements
+**/
+define('ROOT', dirname(__FILE__));
+define('DS', dirname(DIRECTORY_SEPARATOR));
+define('VIEW_PATH', ROOT . DS . 'view' . DS);
+define('MODEL_PATH', ROOT . DS . 'model' . DS);
+define('CONTROLLER_PATH', ROOT . DS . 'controller' . DS);
+define('ASSET_PATH',ROOT . DS . 'assets' . DS);
 
-    <body>
-    <?php
-      include "view/partials/navbar.php";
-    ?>
-
-    <?php
-      include "view/partials/footer.php";
-    ?>
-	</body>
-</html>
+include VIEW_PATH . 'home.php';
+?>
