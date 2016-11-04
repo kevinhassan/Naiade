f81b111356ef0d2c51229af65a784cfe8210c833
kevinhassanig <kevin.hassan@etu.umontpellier.fr>
2016-11-04 11:31:36 +0100

    remove: useless files

--- controller/controlDeconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Je me connecte à la base de données
-    
-        require("../Model/ConnexionBDD.php");
-        // Suppression du cookie user// Set expiration time to -1hr (will cause browser deletion) 
-		 setcookie('user', '', time()-10000000, '/');
-		// Unset key 
-		unset($_COOKIE["user"]);
-		include_once('../Controller/controlIndex.php');
