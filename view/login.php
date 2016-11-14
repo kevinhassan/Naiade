ceb3e2ab7b17e03d72a22bd1315f90bb5fac8f64
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2016-11-14 02:15:36 +0100

    edit: translate english to french

--- view/login.php
+++ view/login.php
@@ -20,7 +20,7 @@ echo <<< EOT
                     <input type="text" class="form-control" placeholder="Email" required autofocus>
                     <input type="password" class="form-control" placeholder="Mot de passe" required>
                     <button class="btn btn-lg btn-primary btn-block" type="submit">
-                        Sign in</button>
+                        Se connecter</button>
                     <label class="checkbox pull-left">
                         <input type="checkbox" value="remember-me">
                         Se souvenir de moi
