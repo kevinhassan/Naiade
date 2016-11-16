013931214834c1b366cf18e921dbb4f8a05f4a15
Godefroi <gofff110@gmail.com>
2016-11-16 01:10:30 +0100

    Modification: de l'api puit

--- controller/addPoint.controller.php
+++ controller/addPoint.controller.php
@@ -1,7 +1,7 @@
 <?php
 
   //API Url
-  $url = 'https://plateforme.api-agro.fr/api/push/1.0/puits/realtime1/push/?pushkey=b4a05c6e96bc4133311393e6233736e03fbabfd2e06456663f9e4a27';
+  $url = 'https://plateforme.api-agro.fr/api/push/1.0/paturage/realtime/push/?pushkey=13b536bd4d5e4d30d110b51a3ae8683d8ec90793819c7276d42b03ed';
 
   //Initiate cURL.
   $ch = curl_init($url);
@@ -15,7 +15,7 @@
   //Execute the request
   $result = curl_exec($ch);
   curl_close($ch);
-  
+
   header('Location: /');
 
 
